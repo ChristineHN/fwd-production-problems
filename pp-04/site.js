903b4ef1d13ca1f0e08b7999dc625d3eac406ae7
christineHN <hnoh3@hawk.iit.edu>
2018-11-12 00:29:27 -0600

    adding functions

--- pp-04/site.js
+++ pp-04/site.js
@@ -3,4 +3,12 @@ function ringDoorbell() {
   doorbell.play();
 }
 
+// rings when the button is clicked
 document.getElementById("bell").addEventListener("click", ringDoorbell);
+
+// bonus task two; rings the door bell when a user clicks "d"
+document.addEventListener("keydown", function(e) {
+  if (e.keyCode == 68) {
+    ringDoorbell();
+  }
+});
