f1f4f9657bc1b36b3b2442ffd2ef45805e78fe00
christineHN <hnoh3@hawk.iit.edu>
2018-11-12 01:38:59 -0600

    adding functions for bonus tasks

--- pp-04/site.js
+++ pp-04/site.js
@@ -1,12 +1,27 @@
+// Prevent users from ringing the doorbell multiple times in a row
+var counter = 0;
+
 function ringDoorbell() {
   var doorbell = new Audio('media/doorbell.mp3');
-  doorbell.play();
+    counter = counter + 1;
+    if(counter == 1){
+      doorbell.play();
+      doorbell.onended = setCounterZero;
+    }
+    else{
+      doorbell.onended = setCounterZero;
+    }
+};
+
+function setCounterZero() {
+  counter = 0;
 }
 
 // rings when the button is clicked
 document.getElementById("bell").addEventListener("click", ringDoorbell);
 
 // bonus task two; rings the door bell when a user clicks "d"
+// d is 68 in ascii
 document.addEventListener("keydown", function(e) {
   if (e.keyCode == 68) {
     ringDoorbell();
